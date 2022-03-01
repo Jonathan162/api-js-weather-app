9fa6b5e7dddcc6104e3a745ad258eafe911aef7a
Joni <rautiojonathan@gmail.com>
2022-03-01 14:03:32 +0100

    getting elements

---
+++ js/app.js
@@ -0,0 +1,12 @@
+("use strict");
+
+//HTML elements
+const input = document.querySelector("#input");
+const SearchBTN = document.querySelector("#search-btn");
+const ClearBTN = document.querySelector("#clear-btn");
+const errDiv = document.querySelector("#error-message");
+const weatherData = document.querySelector("#weather-data");
+const moreWeatherData = document.querySelector("#more-weather-data");
+
+//API key
+const myKey = "";
